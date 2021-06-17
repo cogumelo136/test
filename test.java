6c4e236054ca6e3b94affac8a4a8e2ca736ca199
Diogo Noguti <clz14097@ict.nitech.ac.jp>
2021-06-18 02:08:41 +0900

    change func

--- test.java
+++ test.java
@@ -2,10 +2,10 @@ class test {
     public static void main(String[] args) {
         System.out.println("mergetest");
         System.out.println("add");
-        a();
+        a("func");
     }
 
-    static void a() {
-        System.out.println("a");
+    static void a(String s) {
+        System.out.println(s);
     }
 }
