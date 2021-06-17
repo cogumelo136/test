d976b8732235ab8c562067522f26d62597776877
Diogo Noguti <clz14097@ict.nitech.ac.jp>
2021-06-18 02:02:46 +0900

    add func

--- test.java
+++ test.java
@@ -2,5 +2,10 @@ class test {
     public static void main(String[] args) {
         System.out.println("mergetest");
         System.out.println("add");
+        a();
+    }
+
+    static void a() {
+        System.out.println("a");
     }
 }
