c93b5baef609177b3752ca8bcfaf8fc95d2e4ddb
Diogo Noguti <clz14097@ict.nitech.ac.jp>
2021-06-18 10:07:26 +0900

    commit

--- test.java
+++ test.java
@@ -2,5 +2,10 @@ class test {
     public static void main(String[] args) {
         System.out.println("mergetest");
         System.out.println("add");
+        a("func");
+    }
+
+    static void a(String s) {
+        System.out.println(s);
     }
 }
