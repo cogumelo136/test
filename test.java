97e46a0fc1b9cf9cce2e836169fec2491a98ee1d
Diogo Noguti <clz14097@ict.nitech.ac.jp>
2021-06-18 09:57:27 +0900

    not to conflict

--- test.java
+++ test.java
@@ -1,7 +1,6 @@
 class test {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        System.out.println(a+b);
+        System.out.println("mergetest");
+        System.out.println("add");
     }
 }
